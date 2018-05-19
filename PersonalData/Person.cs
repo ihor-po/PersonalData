84ecfcf0561dd0be9a7b21f6405c391b9a7b32b3
Ihor Porotikov <sobikashi@gmail.com>
2018-05-19 09:46:26 +0300

    first steps

---
+++ PersonalData/Person.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace PersonalData
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string middleName;
+
+        private int age;
+
+        private long inn;
+
+        private string avatar;
+
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string MiddleName
+        {
+            get { return middleName; }
+            set
+            {
+                middleName = value;
+                OnPropertyChanged("MiddleName");
+            }
+        }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        /// <summary>
+        /// ИНН
+        /// </summary>
+        public long Inn
+        {
+            get { return inn; }
+            set
+            {
+                inn = value;
+                OnPropertyChanged("Inn");
+            }
+        }
+
+        /// <summary>
+        /// Аватар
+        /// </summary>
+        public string Avatar
+        {
+            get { return avatar; }
+            set
+            {
+                avatar = value;
+                OnPropertyChanged("Avatar");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Событие изменения
+        /// </summary>
+        /// <param name="name"></param>
+        private void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ PersonalData/ViewModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using System.Collections.ObjectModel; //ObservableCollection
+
+namespace PersonalData
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Person> Persons { get; set; }
+
+        private Person selectedPerson;
+
+        public ViewModel()
+        {
+            Persons = new ObservableCollection<Person>
+            {
+                new Person
+                {
+                    FirstName = "Иван",
+                    MiddleName = "Иванович",
+                    LastName = "Спиридонов",
+                    Age = 45,
+                    Inn = 1231231231,
+                    Avatar = "user.ico"
+                },
+                new Person
+                {
+                    FirstName = "Андорин",
+                    MiddleName = "Адоринов",
+                    LastName = "Лапковский",
+                    Age = 34,
+                    Inn = 2342342342,
+                    Avatar = "Person-purple.png"
+                },
+                new Person
+                {
+                    FirstName = "Ферана",
+                    MiddleName = "Ферановна",
+                    LastName = "Капушкина",
+                    Age = 33,
+                    Inn = 3453453453,
+                    Avatar = "Person_blue.png"
+                },
+                new Person
+                {
+                    FirstName = "Кинурина",
+                    MiddleName = "Адимовна",
+                    LastName = "Вапирки",
+                    Age = 189,
+                    Inn = 4564564564,
+                    Avatar = "Person.png"
+                },
+                new Person
+                {
+                    FirstName = "Петр",
+                    MiddleName = "Петрович",
+                    LastName = "Иванов",
+                    Age = 33,
+                    Inn = 5675675675,
+                    Avatar = "Person-purple.png"
+                },
+                new Person
+                {
+                    FirstName = "Федер",
+                    MiddleName = "Федерович",
+                    LastName = "Петров",
+                    Age = 21,
+                    Inn = 6786786786,
+                    Avatar = "Person_blue.png"
+                },
+                new Person
+                {
+                    FirstName = "Фаралакс",
+                    MiddleName = "Фаралаксович",
+                    LastName = "Фонарев",
+                    Age = 1099,
+                    Inn = 7897897897,
+                    Avatar = "Person.png"
+                }
+            };
+        }
+
+        /// <summary>
+        /// Выбранная персона
+        /// </summary>
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set
+            {
+                selectedPerson = value;
+                OnPropertyChanged("SelectedPerson");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Событие изменения
+        /// </summary>
+        /// <param name="name"></param>
+        private void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
